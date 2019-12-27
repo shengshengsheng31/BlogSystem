4826b55a329795e95d3af87cb83ad6a08d5b2b99
shengshengsheng <1445089856@qq.com>
2019-12-27 21:00:22 +0800

    数据库更改

---
+++ BlogSystem.Models/Migrations/201912231054144_20191223.cs
@@ -0,0 +1,20 @@
+﻿namespace BlogSystem.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _20191223 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Comments", "Content", c => c.String(nullable: false, maxLength: 140));
+            DropColumn("dbo.Comments", "Context");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Comments", "Context", c => c.String(nullable: false, maxLength: 140));
+            DropColumn("dbo.Comments", "Content");
+        }
+    }
+}
